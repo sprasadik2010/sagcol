bef23e6f92da6d96fecdfd175e3424ee9d8c6af7
SIVA PRASAD I K <sprasadik@gmail.com>
2025-07-23 13:06:05 +0530

    New update

--- server/database.py
+++ server/database.py
@@ -1,10 +1,17 @@
 # database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:1q2w3e4r@localhost:5432/sagcol"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")  # from .env, must be the full Neon connection string
+
+# IMPORTANT: Add `?sslmode=require` to work with Neon
+if "sslmode" not in DATABASE_URL:
+    DATABASE_URL += "?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()

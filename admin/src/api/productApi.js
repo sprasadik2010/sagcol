f43df427caa9d5f796808dc4c5144337e18051a2
SIVA PRASAD I K <sprasadik@gmail.com>
2025-07-23 14:29:19 +0530

    New update

--- admin/src/api/productApi.js
+++ admin/src/api/productApi.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000/products"; // FastAPI backend
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const fetchProducts = async () => {
   const res = await fetch(API_URL + "/GetAllProducts");
